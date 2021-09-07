1c9d01f83a898713b9b47b4057788b73c0a4e784
Баннов Филипп Германович <211901@mck-ktits.ru>
2021-09-07 12:27:20 +0300

    Добавьте файлы проекта.

---
+++ bank1/Operation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bank1
+{
+    class Operation
+    {
+        public static void ShowBalance(Account acc)
+        {
+            Console.WriteLine(acc.Balance);
+        }
+        public static void Transaction(Account acc, Account acc1, double sum)
+        {
+            acc.Balance -= sum;
+            acc1.Balance += sum;
+            Console.WriteLine("Transaction completed");
+        }
+        public static int Widthraw(Account account, double sum)
+        {
+            if (sum % 1 < 1)
+            {
+                account.Balance -= sum;
+            }
+            return 0;
+        }
+        public static int Deposit(Account accountgetter , double sum)
+        {
+            accountgetter.Balance += sum;
+            Console.WriteLine("Deposit comleted");
+            return 0;
+        }
+        public static int Credit(Account aaccountgettercc, double Sum)
+        {
+            return 0;
+        }
+    }
+}
---
+++ bank1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace bank1
+{
+    class Programbalance
+    {
+        static void Main(string[] args)
+        {
+            Account acc1 = new Account();
+            Account acc2 = new Account();
+            acc1.Balance = 10000;
+            acc2.Balance = 1000;
+            Operation.ShowBalance(acc1);
+            Operation.ShowBalance(acc2);
+            Operation.Transaction(acc1, acc2, 500);
+            Operation.Widthraw(acc1, acc2, 800);
+            Operation.Deposit(acc1, acc2, 1000);
+        }
+    }
+}
